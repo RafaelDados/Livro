6d4c607de69ec796e51027b25c5d1a614b6597c1
Rafael <Rafael@PC>
2018-04-04 13:24:07 -0300

    classes conta,conta corrente e conta poupanca adicionadas.

---
+++ postgresql/classes/ContaCorrente.php
@@ -0,0 +1,20 @@
+<?php
+
+class ContaCorrente extends Conta {
+
+    protected $limite;
+
+    function __construct($agencia, $conta, $saldo, $limite) {
+        parent::__construct($agencia, $conta, $saldo);
+        $this->limite = $limite;
+    }
+
+    public function retirar($quantia) {
+        if (($this->saldo + $this->limite) >= $quantia) {
+            $this->saldo -= $quantia;
+        } else {
+            return false;
+        }
+        return true;
+    }
+}
